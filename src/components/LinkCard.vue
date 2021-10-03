6acde2cdd69ac16837dedd0f91b7d86933963181
dev <devjutzu@gmail.com>
2021-10-03 19:49:35 -0400

    fixed mobile view

--- src/components/LinkCard.vue
+++ src/components/LinkCard.vue
@@ -2,25 +2,24 @@
   <v-card>
     <v-container>
       <v-row
-        ><v-col cols="1"
-          ><v-icon x-large>{{ link.icon }}</v-icon></v-col
+        ><v-col cols="12" lg="1"
+          ><v-icon x-large>{{ link.icon }}</v-icon>
+          <v-divider></v-divider>
+          <v-btn @click="deleteLink(link.id)" icon>
+            <v-icon>mdi-trash-can</v-icon>
+          </v-btn>
+          <!-- <v-btn to="/edit/" icon> <v-icon>mdi-pencil</v-icon> </v-btn> -->
+
+          <v-btn :href="link.link" target="_blank" icon>
+            <v-icon>mdi-arrow-right-bold</v-icon>
+          </v-btn></v-col
         ><v-divider vertical></v-divider
-        ><v-col cols="7"
+        ><v-col cols="12" lg="7"
           ><v-card-title> {{ link.name }} </v-card-title
           ><v-card-title>{{ link.link }}</v-card-title></v-col
         >
         <v-divider vertical></v-divider>
-        <v-col cols="3"> <QR :options="link.options" /> </v-col>
-        <v-divider vertical />
-        <v-col cols="1">
-          <v-btn @click="deleteLink(link.id)" icon>
-            <v-icon>mdi-trash-can</v-icon>
-          </v-btn>
-          <v-btn :href="link.link" target="_blank" icon>
-            <v-icon>mdi-arrow-right-bold</v-icon>
-          </v-btn>
-          <!-- <v-btn to="/edit/" icon> <v-icon>mdi-pencil</v-icon> </v-btn> -->
-        </v-col>
+        <v-col cols="12" lg="4"> <QR :options="link.options" /> </v-col>
       </v-row>
     </v-container>
   </v-card>
